c28d21686f200fda26dedb9dc6e85d1882769d64
Nick Behrens <behrensn@iastate.edu>
2017-06-18 15:46:22 -0500

    Initial Commit

---
+++ app/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    verify = require('./verify'),
+    forecaster = require('./forecaster');
+
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json({
+    verify: function getRawBody(req, res, buf) {
+        req.rawBody = buf.toString();
+    }
+}));
+
+
+app.get('/', function(req, res) {
+    res.json({ message: 'The forecaster is up and running.', since: (new Date()).toString() });
+});
+app.post('/lyrics', verify, forecaster);
+
+
+app.listen(app.get('port'), function() {
+    console.log('Forecaster is up and running on port %d', app.get('port'));
+});
---
+++ app/forecaster.js
@@ -0,0 +1,59 @@
+'use strict';
+
+
+module.exports = function(req, res) {
+
+    console.log('New request for the forecaster:\n', req.body);
+
+    if (req.body.request.type === 'IntentRequest' &&
+               req.body.request.intent.name === 'GetLyrics') {
+
+        if (req.body.request.intent.slots.Lyrics &&
+            req.body.request.intent.slots.Lyrics.value) {
+
+            getLyrics(req.body.request.intent.slots.Lyrics.value)
+              .then(function(song)){
+                console.log('responding to Lyric request for ' + req.body.request.intent.slots.Lyrics.value + ' with ', song);
+                res.json(
+                    buildResponse( {}, '<speak>' + song + '</speak>')
+                );
+                .catch(function(err) {
+                    res.json(
+                        buildResponse( {}, '<speak>' + err + '</speak>', {}, true )
+                    );
+              }
+
+            }
+        }
+
+    } else {
+        console.error('Intent not implemented: ', req.body);
+        res.status(504).json({ message: 'Intent Not Implemented' });
+    }
+
+};
+
+
+function buildResponse(session, speech, card, end) {
+    return {
+        version: VERSION,
+        sessionAttributes: session,
+        response: {
+            outputSpeech: {
+                type: 'SSML',
+                ssml: speech
+            }
+        }
+    };
+}
+
+function getLyrics(lyrics){
+  var api = require('genius-api');
+  var genius = new api("dXif373aX9mzBQRvThIKy5Jm25hcvzaGjz9FA7_TVuZp3Ke5DKuCCJC1eK17urdv");
+
+  //search
+  genius.search(lyrics).then(function(response) {
+    console.log('hits', response.hits[0].result.full_title);
+    return response.hits[0].result.full_title;
+  });
+}
